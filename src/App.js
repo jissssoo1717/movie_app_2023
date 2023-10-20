94e4a740daec4daee7d08dfd1c0ae5fb2b7ef844
jissssoo1717 <jisoolee7101@gmail.com>
2023-10-20 15:35:07 +0900

    Reusable Components with JSX + Props

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React from "react";
 
+function Food({ fav }) {
+  return <h1>I like {fav}</h1>;
+}
+
 function App() {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <h1>Hello</h1>
+      <Food fav="kimchi" />
+      <Food fav="ramen" />
+      <Food fav="samgiopsal" />
+    </div>
+  );
 }
 
 export default App;
