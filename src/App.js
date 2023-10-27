ac580d5e2a8f9bd214f54dc8bb96102902bdd9bc
jissssoo1717 <jisoolee7101@gmail.com>
2023-10-27 15:24:55 +0900

    Component life cycle

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import React from "react";
-import PropTypes from "prop-types";
 
 class App extends React.Component {
   state = {
-    count: 0,
-  };
-  add = () => {
-    console.log("add");
-  };
-  minus = () => {
-    console.log("minus");
+    isLoading: true,
+    movies: [],
   };
 
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ isLoading: false });
+    }, 6000);
+  }
   render() {
-    return (
-      <div>
-        <h1>The number is: {this.state.count}</h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
-      </div>
-    );
+    const { isLoading } = this.state;
+    return <div>{isLoading ? "Loading..." : "We are ready"}</div>;
   }
 }
 export default App;
