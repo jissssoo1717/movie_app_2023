8484e5c730fda16d80563cdfea7e28603ac7d868
jissssoo1717 <jisoolee7101@gmail.com>
2023-10-31 14:04:58 +0900

    Styling(fix after)

--- src/Movie.js
+++ src/Movie.js
@@ -4,19 +4,21 @@ import "./Movie.css";
 
 function Movie({ year, title, summary, poster, genres }) {
   return (
-    <div className="movies__movie">
-      <img src={poster} alt={title} title={title} />
-      <div className="movie__data">
-        <h3 className="movie__title">{title}</h3>
-        <span className="movie__year">{year}</span>
-        <ul className="movie__genres">
-          {genres.map((genre, index) => (
-            <li key={index} className="genres__genre">
-              {genre}
-            </li>
-          ))}
-        </ul>
-        <p className="movie__summary">{summary.slice(0, 180)}...</p>
+    <div className="movies">
+      <div className="movie">
+        <img src={poster} alt={title} title={title} />
+        <div className="movie__data">
+          <h3 className="movie__title">{title}</h3>
+          <span className="movie__year">{year}</span>
+          <ul className="movie__genres">
+            {genres.map((genre, index) => (
+              <li key={index} className="genres__genre">
+                {genre}
+              </li>
+            ))}
+          </ul>
+          <p className="movie__summary">{summary.slice(0, 180)}...</p>
+        </div>
       </div>
     </div>
   );
