f6e3941e642903b1c8c9bd0cad56a68023c1c7d0
jissssoo1717 <jisoolee7101@gmail.com>
2023-11-06 14:47:21 +0900

    Movie Detail  Clean up

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -11,21 +11,25 @@ class Detail extends React.Component {
     const { location } = this.props;
     if (location.state) {
       return (
-        <div>
+        <div className="container">
+          <section className="detail">
+            <h1 className="detail__title">{location.state.title}</h1>
+            <span className="detail__year">{location.state.year}</span>
+            <span className="detail__rating">{location.state.rating}</span>
+            <ul className="detail__genres">
+              {location.state.genres.map((genre, index) => (
+                <li key={index}>{genre}</li>
+              ))}
+            </ul>
+            <p className="detail__summary">{location.state.summary}</p>
+          </section>
+
           <img
+            className="detail__poster"
             src={location.state.poster_big}
             alt={location.state.title}
             title={location.state.title}
           />
-          <h1>{location.state.title}</h1>
-          <span>{location.state.year}</span>
-          <span>rating: {location.state.rating}</span>
-          <ul>
-            {location.state.genres.map((genre, index) => (
-              <li key={index}>{genre}</li>
-            ))}
-          </ul>
-          <p>{location.state.summary}</p>
         </div>
       );
     } else {
